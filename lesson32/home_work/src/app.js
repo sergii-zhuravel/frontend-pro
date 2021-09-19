455f0eea8aa2564581218c960833cd787ed5de37
Serhii Zhuravel <szhuravell@gmail.com>
2021-09-19 18:16:09 +0300

    Added common files

--- lesson32/home_work/src/app.js
+++ lesson32/home_work/src/app.js
@@ -1,6 +1,6 @@
-import '../../common/css/normalize.css'
-import '../../common/css/skeleton.css'
-import '../../common/css/dark-theme.css'
+import './common/css/normalize.css'
+import './common/css/skeleton.css'
+import './common/css/dark-theme.css'
 import './styles.css'
 
 import $ from 'jquery';
